071e32dcba2bd9fee56571878a4593c49a42813c
Jonathan Mendler <Jon.mendler@gmail.com>
2023-05-12 12:35:07 -0500

    Created items

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Song.create(title: "Vivien", album: "PERMANENT.RADIANT", artist: "Crosses", year: 2022)
+
+Song.create(title: "Toni", album: "The Other Side of Make Believe", artist: "Interpol", year: 2022)
+
+Song.create(title: "Long Flight", album: "Memorabilia", artist: "Human Tetris", year: 2018)
+
+Song.create(title: "Good Mistake", album: "Pocketknife", artist: "Mr. Little Jeans", year: 2014)
