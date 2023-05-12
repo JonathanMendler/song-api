9c2ea5b8d1ff8d2197801c89247a4eaac03d20e9
Jonathan Mendler <Jon.mendler@gmail.com>
2023-05-12 13:32:30 -0500

    Added test data

--- test/controllers/songs_controller_test.rb
+++ test/controllers/songs_controller_test.rb
@@ -4,4 +4,11 @@ class SongsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test "index" do
+    get "/songs.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal Song.count, data.length
+  end
 end
